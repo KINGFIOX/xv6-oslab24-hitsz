5837ad911231936b17dec421abd8789aeb973622
wangfiox <wangfiox@gmail.com>
2024-10-02 04:28:30 +0800

    :feat: 最小堆

--- kernel/bio.c
+++ kernel/bio.c
@@ -26,6 +26,78 @@
 
 typedef struct buf buf_t;
 
+/* ---------- ---------- heap ---------- ---------- */
+
+typedef struct {
+  uint timestamp;
+  buf_t *pointer;
+} entry_t;
+
+typedef struct {
+  int size;
+  entry_t array[NBUF];
+} MinHeap;
+
+// 交换两个 entry_t 的值
+void swap(entry_t *x, entry_t *y) {
+  entry_t temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void heapifyUp(MinHeap *heap, int idx) {
+  int parent = (idx - 1) / 2;
+  if (idx && heap->array[parent].timestamp > heap->array[idx].timestamp) {
+    swap(&heap->array[parent], &heap->array[idx]);
+    heapifyUp(heap, parent);
+  }
+}
+
+void heapifyDown(MinHeap *heap, int idx) {
+  int left = 2 * idx + 1;
+  int right = 2 * idx + 2;
+  int smallest = idx;
+  if (left < heap->size && heap->array[left].timestamp < heap->array[smallest].timestamp) smallest = left;
+  if (right < heap->size && heap->array[right].timestamp < heap->array[smallest].timestamp) smallest = right;
+  if (smallest != idx) {
+    swap(&heap->array[smallest], &heap->array[idx]);
+    heapifyDown(heap, smallest);
+  }
+}
+
+void insertMinHeap(MinHeap *heap, entry_t key) {
+  if (heap->size == NBUF) panic("heap full\n");
+  heap->array[heap->size] = key;
+  heap->size++;
+  heapifyUp(heap, heap->size - 1);
+}
+
+entry_t extractMin(MinHeap *heap) {
+  if (heap->size <= 0) {
+    entry_t empty = {0xffffffff, 0};
+    return empty;
+  }
+  if (heap->size == 1) {
+    heap->size--;
+    return heap->array[0];
+  }
+  entry_t root = heap->array[0];
+  heap->array[0] = heap->array[heap->size - 1];
+  heap->size--;
+  heapifyDown(heap, 0);
+  return root;
+}
+
+entry_t getMin(MinHeap *heap) {
+  if (heap->size <= 0) {
+    entry_t empty = {0xffffffff, 0};
+    return empty;
+  }
+  return heap->array[0];
+}
+
+/* ---------- ---------- heap ---------- ---------- */
+
 typedef struct {
   struct spinlock lock;
   buf_t head;  // 已分配
@@ -33,7 +105,7 @@ typedef struct {
 
 struct {
   struct spinlock freelock;
-  buf_t free;  // 空闲
+  MinHeap freelist;  // 空闲
 
   buf_t buf[NBUF];
 
@@ -43,24 +115,19 @@ struct {
 void binit(void) {
   initlock(&bcache.freelock, "freelock");
 
+  bcache.freelist.size = 0;
+
   for (int i = 0; i < NBUCKET; i++) {
     initlock(&bcache.buckets[i].lock, "bucket");
     bcache.buckets[i].head.next = 0;
     bcache.buckets[i].head.prev = 0;
-    bcache.free.next = 0;
-    bcache.free.prev = 0;
   }
 
   // Create linked list of buffers
   for (int i = 0; i < NBUF; i++) {
     initsleeplock(&bcache.buf[i].lock, "buffer");
-    bcache.buf[i].next = bcache.free.next;
-    bcache.buf[i].prev = &bcache.free;
-    bcache.buf[i].timestamp = 0;
-    bcache.free.next = &bcache.buf[i];
-    if (bcache.buf[i].next) {
-      bcache.buf[i].next->prev = &bcache.buf[i];
-    }
+    entry_t entry = {0, &bcache.buf[i]};
+    insertMinHeap(&bcache.freelist, entry);
   }
 }
 
@@ -69,6 +136,21 @@ void binit(void) {
 // In either case, return locked buffer.
 static struct buf *bget(uint dev, uint blockno) {
   int hash = blockno % NBUCKET;
+
+  // for (buf_t *cur = bcache.buckets[hash].head.next; cur; cur = cur->next) {
+  //   if (cur->dev == dev && cur->blockno == blockno) {
+  //     acquire(&bcache.buckets[hash].lock);
+  //     if (cur->dev == dev && cur->blockno == blockno) {
+  //       cur->refcnt++;
+  //       cur->timestamp = ticks;
+  //       release(&bcache.buckets[hash].lock);  // 找到, 解锁, 返回
+  //       acquiresleep(&cur->lock);
+  //       return cur;
+  //     }
+  //     release(&bcache.buckets[hash].lock);
+  //   }
+  // }
+
   acquire(&bcache.buckets[hash].lock);
   for (buf_t *cur = bcache.buckets[hash].head.next; cur; cur = cur->next) {
     if (cur->dev == dev && cur->blockno == blockno) {
@@ -82,28 +164,15 @@ static struct buf *bget(uint dev, uint blockno) {
 
 retry:
   acquire(&bcache.freelock);
-  buf_t *iter = bcache.free.next;
-  if (iter == 0) {
+  entry_t iter = getMin(&bcache.freelist);
+  if (iter.pointer == 0) {
     release(&bcache.freelock);
     goto retry;  // 有点自旋的意思
   }
-  buf_t *cur = iter;
-  uint lru = ticks;
-  for (; iter; iter = iter->next) {
-    if (iter->timestamp == 0) {
-      break;
-      cur = iter;
-    } else if (iter->timestamp < lru) {
-      lru = iter->timestamp;
-      cur = iter;
-    }
-  }
+  iter = extractMin(&bcache.freelist);
+  buf_t *cur = iter.pointer;
 
   // pop cur from free
-  if (cur->next != 0) {
-    cur->next->prev = cur->prev;
-  }
-  bcache.free.next = cur->next;
   release(&bcache.freelock);
 
   cur->dev = dev;  // init
@@ -113,20 +182,20 @@ retry:
   cur->timestamp = ticks;
 
   // push cur to head
-  cur->next = bcache.buckets[blockno % NBUCKET].head.next;
-  cur->prev = &bcache.buckets[blockno % NBUCKET].head;
-  bcache.buckets[blockno % NBUCKET].head.next = cur;
+  cur->next = bcache.buckets[hash].head.next;
+  cur->prev = &bcache.buckets[hash].head;
+  bcache.buckets[hash].head.next = cur;
   if (cur->next) {
     cur->next->prev = cur;
   }
-  release(&bcache.buckets[blockno % NBUCKET].lock);
+  release(&bcache.buckets[hash].lock);
 
   acquiresleep(&cur->lock);
   return cur;
 
   // failed
   release(&bcache.freelock);
-  release(&bcache.buckets[blockno % NBUCKET].lock);
+  release(&bcache.buckets[hash].lock);
   panic("bget: no buffers");
 }
 
@@ -151,9 +220,11 @@ void bwrite(struct buf *b) {
 void brelse(struct buf *b) {
   if (!holdingsleep(&b->lock)) panic("brelse");
 
+  int hash = b->blockno % NBUCKET;
+
   releasesleep(&b->lock);
 
-  acquire(&bcache.buckets[b->blockno % NBUCKET].lock);
+  acquire(&bcache.buckets[hash].lock);
   b->refcnt--;
   if (b->refcnt == 0) {
     if (b->next) {
@@ -162,15 +233,13 @@ void brelse(struct buf *b) {
     b->prev->next = b->next;
 
     acquire(&bcache.freelock);
-    b->next = bcache.free.next;
-    b->prev = &bcache.free;
-    bcache.free.next = b;
-    if (b->next) {
-      b->next->prev = b;
-    }
+    entry_t entry = {ticks, b};
+    b->next = 0;
+    b->prev = 0;
+    insertMinHeap(&bcache.freelist, entry);
     release(&bcache.freelock);
   }
-  release(&bcache.buckets[b->blockno % NBUCKET].lock);
+  release(&bcache.buckets[hash].lock);
 }
 
 void bpin(struct buf *b) {
