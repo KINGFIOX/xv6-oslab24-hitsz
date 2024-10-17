be49bda45a739d3a9faa23fa2b0b194cc1eeaed0
wangfiox <wangfiox@gmail.com>
2024-10-17 15:21:08 +0800

    :wrench: + :art: reformat

--- kernel/sysnet.c
+++ kernel/sysnet.c
@@ -15,34 +15,26 @@
 #include "net.h"
 
 struct sock {
-  struct sock *next; // the next socket in the list
-  uint32 raddr;      // the remote IPv4 address
-  uint16 lport;      // the local UDP port number
-  uint16 rport;      // the remote UDP port number
-  struct spinlock lock; // protects the rxq
-  struct mbufq rxq;  // a queue of packets waiting to be received
+  struct sock *next;     // the next socket in the list
+  uint32 raddr;          // the remote IPv4 address
+  uint16 lport;          // the local UDP port number
+  uint16 rport;          // the remote UDP port number
+  struct spinlock lock;  // protects the rxq
+  struct mbufq rxq;      // a queue of packets waiting to be received
 };
 
 static struct spinlock lock;
 static struct sock *sockets;
 
-void
-sockinit(void)
-{
-  initlock(&lock, "socktbl");
-}
+void sockinit(void) { initlock(&lock, "socktbl"); }
 
-int
-sockalloc(struct file **f, uint32 raddr, uint16 lport, uint16 rport)
-{
+int sockalloc(struct file **f, uint32 raddr, uint16 lport, uint16 rport) {
   struct sock *si, *pos;
 
   si = 0;
   *f = 0;
-  if ((*f = filealloc()) == 0)
-    goto bad;
-  if ((si = (struct sock*)kalloc()) == 0)
-    goto bad;
+  if ((*f = filealloc()) == 0) goto bad;
+  if ((si = (struct sock *)kalloc()) == 0) goto bad;
 
   // initialize objects
   si->raddr = raddr;
@@ -59,9 +51,7 @@ sockalloc(struct file **f, uint32 raddr, uint16 lport, uint16 rport)
   acquire(&lock);
   pos = sockets;
   while (pos) {
-    if (pos->raddr == raddr &&
-        pos->lport == lport &&
-	pos->rport == rport) {
+    if (pos->raddr == raddr && pos->lport == lport && pos->rport == rport) {
       release(&lock);
       goto bad;
     }
@@ -73,16 +63,12 @@ sockalloc(struct file **f, uint32 raddr, uint16 lport, uint16 rport)
   return 0;
 
 bad:
-  if (si)
-    kfree((char*)si);
-  if (*f)
-    fileclose(*f);
+  if (si) kfree((char *)si);
+  if (*f) fileclose(*f);
   return -1;
 }
 
-void
-sockclose(struct sock *si)
-{
+void sockclose(struct sock *si) {
   struct sock **pos;
   struct mbuf *m;
 
@@ -90,7 +76,7 @@ sockclose(struct sock *si)
   acquire(&lock);
   pos = &sockets;
   while (*pos) {
-    if (*pos == si){
+    if (*pos == si) {
       *pos = si->next;
       break;
     }
@@ -104,12 +90,10 @@ sockclose(struct sock *si)
     mbuffree(m);
   }
 
-  kfree((char*)si);
+  kfree((char *)si);
 }
 
-int
-sockread(struct sock *si, uint64 addr, int n)
-{
+int sockread(struct sock *si, uint64 addr, int n) {
   struct proc *pr = myproc();
   struct mbuf *m;
   int len;
@@ -126,8 +110,7 @@ sockread(struct sock *si, uint64 addr, int n)
   release(&si->lock);
 
   len = m->len;
-  if (len > n)
-    len = n;
+  if (len > n) len = n;
   if (copyout(pr->pagetable, addr, m->head, len) == -1) {
     mbuffree(m);
     return -1;
@@ -136,15 +119,12 @@ sockread(struct sock *si, uint64 addr, int n)
   return len;
 }
 
-int
-sockwrite(struct sock *si, uint64 addr, int n)
-{
+int sockwrite(struct sock *si, uint64 addr, int n) {
   struct proc *pr = myproc();
   struct mbuf *m;
 
   m = mbufalloc(MBUF_DEFAULT_HEADROOM);
-  if (!m)
-    return -1;
+  if (!m) return -1;
 
   if (copyin(pr->pagetable, mbufput(m, n), addr, n) == -1) {
     mbuffree(m);
@@ -155,9 +135,7 @@ sockwrite(struct sock *si, uint64 addr, int n)
 }
 
 // called by protocol handler layer to deliver UDP packets
-void
-sockrecvudp(struct mbuf *m, uint32 raddr, uint16 lport, uint16 rport)
-{
+void sockrecvudp(struct mbuf *m, uint32 raddr, uint16 lport, uint16 rport) {
   //
   // Find the socket that handles this mbuf and deliver it, waking
   // any sleeping reader. Free the mbuf if there are no sockets
@@ -168,8 +146,7 @@ sockrecvudp(struct mbuf *m, uint32 raddr, uint16 lport, uint16 rport)
   acquire(&lock);
   si = sockets;
   while (si) {
-    if (si->raddr == raddr && si->lport == lport && si->rport == rport)
-      goto found;
+    if (si->raddr == raddr && si->lport == lport && si->rport == rport) goto found;
     si = si->next;
   }
   release(&lock);
