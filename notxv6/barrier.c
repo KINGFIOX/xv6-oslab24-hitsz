560062068ba5c5acbbce1d023dd7d170673fc13d
wangfiox <wangfiox@gmail.com>
2024-10-17 14:56:07 +0800

    barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -26,6 +26,17 @@ static void barrier() {
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  if (++bstate.nthread == nthread) {
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    while (bstate.nthread != nthread) {
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+  }
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *thread(void *xa) {
